30488a7007a1913ebde987dbf87c97592150c1e1
Urluescu, Alexandre Joao (uif94707) <uif94707@contiwan.com>
2023-09-07 14:43:23 +0300

    Make the input more friendly and prettier

--- client/src/components/ButtonCustom.jsx
+++ client/src/components/ButtonCustom.jsx
@@ -4,7 +4,7 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 const ButtonCustom = ({ textName, eventFunction, classStyle, typeButton }) => {
   return (
-    <button type={typeButton} className={classStyle}><FontAwesomeIcon icon={faPaperPlane} /></button>
+    <button type={typeButton} className={classStyle}><FontAwesomeIcon style={{fontSize: "30px"}} icon={faPaperPlane} /></button>
   )
 }
 
