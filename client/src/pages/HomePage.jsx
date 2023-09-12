31d49d112ef49fd30e1bb3ef0b587ce7fc9d1638
Urluescu, Alexandre Joao (uif94707) <uif94707@contiwan.com>
2023-09-12 17:42:40 +0300

    Create style for the homePage planet

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from "react";
+
+import "../style/HomePage.css";
 
 // Components -------------------------------
-import NavBar from '../components/NavBar'
+import NavBar from "../components/NavBar";
 
 const HomePage = () => {
   return (
     <div>
-        <NavBar />
-       <div>Home</div>
+      <NavBar />
+      <div className="title">Welcome in Terra</div>
+      <div className="circleContainer">
+        <div className="circleContainerStyle">
+          <div className="circle">
+            <div className="circle1">1</div>
+            <div className="circle2">2</div>
+            <div className="circle3">3</div>
+            <div className="circle4">4</div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
