efdebf46148c2eac6c8c8124ac0f38e38bbfc1a1
Urluescu, Alexandre Joao (uif94707) <uif94707@contiwan.com>
2023-09-12 09:25:51 +0300

    Implement the router into the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 // import { useEffect } from 'react';
 import "./App.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 // PAGES ----------------------------------
 import ChatPage from "./pages/ChatPage";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <div className="App">
-      <div className="App">
-        {/* <NavBar /> */}
-        <ChatPage />
-        {/* <Footer /> */}
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contiChat" element={<ChatPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
