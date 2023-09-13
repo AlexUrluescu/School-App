178690b0d80c10b13839a247877a53ce80a19b79
Urluescu, Alexandre Joao (uif94707) <uif94707@contiwan.com>
2023-09-13 09:41:22 +0300

    Finish the style of the planet

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,21 +2,25 @@ import React from "react";
 
 import "../style/HomePage.css";
 
+import image from "../static/planet.png"
+
 // Components -------------------------------
 import NavBar from "../components/NavBar";
 
 const HomePage = () => {
   return (
     <div>
-      <NavBar />
-      <div className="title">Welcome in Terra</div>
+      {/* <NavBar /> */}
+      <div className="title"><h1>Welcome in Terra</h1></div>
       <div className="circleContainer">
         <div className="circleContainerStyle">
           <div className="circle">
-            <div className="circle1">1</div>
-            <div className="circle2">2</div>
-            <div className="circle3">3</div>
-            <div className="circle4">4</div>
+            <img className="planet_foto" src={image} alt="planet" />
+
+            <div className="circle1">1st year</div>
+            <div className="circle2">2nd year</div>
+            <div className="circle3">3rd yeard</div>
+            <div className="circle4">4th year</div>
           </div>
         </div>
       </div>
